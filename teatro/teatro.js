c5bb9173f0305303e282babce1cdceaaaa9a5a09
nicoleinae <nicoleinae@gmail.com>
2023-09-12 22:30:30 -0300

    terminando aula 12/09

--- teatro/teatro.js
+++ teatro/teatro.js
@@ -7,12 +7,31 @@ const POLTRONAS = 240; //quantidade toral de poltronas
 const reservadas = []; //vetor com as poltronas reservadas
 
 window.addEventListener("load", () => { //ao carregar a janela...
+    const ocupadas = localStorage.getItem("teatroOcupadas")
+        ? localStorage.getItem("teatroOcupadas").split(";")
+        : []; //split para inserir o ";"
 
     for(let i=1; i<=POLTRONAS; i++) {
-        const figura = document.createElement("figure"); //cria a tag figure
-        const imgStatus = document.createElement("img"); // ||
+        const figure = document.createElement("figure"); //cria a tag "figure"
+        const imgStatus = document.createElement("img");
+
+        imgStatus.src = ocupadas.includes(i.toString()) 
+            ? "img/ocupada.jpg"
+            : "img/disponivel.jpg";
+        imgStatus.className = "Poltrona"
 
         const figureCap = document.createElement("figcaption")
+
+        //quantidade de zeros antes do numero da poltrona
+        const zeros = i < 10 ? "00" : i < 100 ? "0" : "";
+        // cria o número da poltrona
+        const num = document.createTextNode(`[${zeros}${i}]`);
+
+        figureCap.appendChild(num);
+        figure.appendChild(imgStatus);
+        figure.appendChild(figureCap);
+
+        dvPalco.appendChild(figure);
     }
 
-})
+});
